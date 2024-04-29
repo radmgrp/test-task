2040743cc1e4e463864e0f38a959eefbc4d75ce8
Mikhail Shayunou <radmgrp@gmail.com>
2024-04-29 23:25:47 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# test-task
+## Step 1: Build Operator Docker Image
+
+Build the Docker image with the following command:
+```bash
+docker build . -t radmgrp/operator-test:latest
+```
+## Step 2: Push Docker Image
+
+Push the Docker image to the registry:
+```bash
+docker push radmgrp/operator-test:latest
+```
+
+## Step 3: Deploy Helm Chart
+
+Upgrade or install the Helm chart with the following command:
+```bash
+helm upgrade --install --create-namespace -n system operator ./operator-helm
+```
+## Step 4: Create Namespace
+
+Create a Kubernetes namespace for the application:
+```bash
+kubectl create ns app
+```
+## Step 4: Apply Sample Custom Resource
+
+Apply the custom resource YAML file in the newly created namespace:
+```bash
+kubectl apply -n app -f sample/cr-sample.yaml
+```
---
+++ app/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port      string
+	TimeRange int
+	Hosts     []string
+}
+
+func main() {
+
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+
+	// Start the HTTP server
+	http.HandleFunc("/ping", pingHandler)
+	go func() {
+		if err := http.ListenAndServe(config.Port, nil); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Invoke other hosts' GET /ping route every Nth amount of time
+	go func() {
+		for {
+			os.Clearenv()
+			config, err = loadConfig()
+			if err != nil {
+				log.Fatalf("error loading config: %v", err)
+			}
+			for _, host := range config.Hosts {
+				hostName := strings.Split(host, ":")
+				ips, err := net.LookupIP(hostName[0])
+				fmt.Printf("%v ips connected to the service %v found.\n", len(ips), hostName[0])
+				for _, ip := range ips {
+					fmt.Printf("contact ip: %v\n", ip.String()+":"+hostName[1])
+					resp, err := http.Get(fmt.Sprintf("http://%s/ping", ip.String()+":"+hostName[1]))
+					if err != nil {
+						fmt.Printf("Error connecting to %s: %v\n", host, err)
+						continue
+					}
+					defer resp.Body.Close()
+					fmt.Printf("Response from %s: %s\n", ip.String()+":"+hostName[1], readResponseBody(resp))
+				}
+
+				if err != nil {
+					fmt.Printf("unexpected error while looking up ips: %v", err)
+				}
+			}
+			fmt.Println("Wait " + strconv.Itoa(config.TimeRange) + " seconds")
+			time.Sleep(time.Duration(config.TimeRange) * time.Second)
+		}
+	}()
+
+	fmt.Println("Server started at :" + config.Port)
+	fmt.Println("Ping period: " + strconv.Itoa(config.TimeRange))
+	select {}
+}
+
+// Helper function to load config
+
+func loadConfig() (Config, error) {
+
+	err := godotenv.Load("/config/.env")
+	//err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	port := ":" + os.Getenv("port")
+	hostsStr := os.Getenv("hosts")
+	timeRange, err := strconv.Atoi(os.Getenv("timeRange"))
+
+	hostsSlice := strings.Split(hostsStr, ",")
+	var hosts []string
+	for _, h := range hostsSlice {
+		hosts = append(hosts, strings.TrimSpace(h))
+	}
+
+	config := Config{
+		Port:      port,
+		TimeRange: timeRange,
+		Hosts:     hosts,
+	}
+
+	return config, nil
+
+}
+
+// Handler for GET /ping route
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "pong")
+}
+
+// Helper function to read response body
+
+func readResponseBody(resp *http.Response) string {
+	buf := make([]byte, 1024)
+	n, _ := resp.Body.Read(buf)
+	return string(buf[:n])
+}
